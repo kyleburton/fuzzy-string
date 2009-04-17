0d1cf741a48625f20d9dee99092b1c18361ed908
Kyle Burton <mortis@indigo64.(none)>
2009-04-16 21:54:41 -0400

    test code for levenshtein, implemented nysiis

---
+++ levenshtein.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'text'
+
+# Text::Levenshtein.distance(left,right)
+# Text::Soundex.soundex(left,right)
+# Text::Metaphone.double_metaphone(left,right)
+
+def edit_distance(left,right)
+  if left.size < right.size
+    left, right = right, left
+  end
+  Text::Levenshtein.distance(left,right)
+end
+
+def edit_distance_sim(left,right)
+  if left.size < right.size
+    left, right = right, left
+  end
+  edits = Text::Levenshtein.distance(left,right)
+  ((left.size - edits) / (1.0 * left.size))
+end
+
+
+target = "De Morgan"
+words = ["De Morgan", "D'Morgun", "D'Morgun", "Demorgyn", "De Murgen", "Dy Moregan", "Dy Murgan", "Da Murgan", "Da Morgan", "Da Myrgn"];
+
+words.each {|word|
+  puts "#{target} vs #{word} => #{edit_distance(target,word)} => #{edit_distance_sim(target,word)}"
+}
---
+++ nysiis.rb
@@ -0,0 +1,47 @@
+def nysiis(string)
+  string = string.upcase
+  
+  string.gsub! /[^A-Za-z]/, ''
+  string.gsub! /[SZ]*$/, ''
+  string.sub! /^MAC/, 'MC'
+  string.sub! /^PF/, 'F'
+
+  string.sub! /IX$/, 'IC'
+  string.sub! /EX$/, 'EC'
+  string.sub! /(?:YE|EE|IE)$/, 'Y'
+  string.sub! /(?:NT|ND)$/, 'N'
+
+  string.gsub! /(.)EV/, "#{$1}EF"
+  first = string[0..0]
+  string.gsub! /[AEIOU]+/, 'A'
+  string.gsub! /AW/, 'A'
+    
+  string.gsub! /GHT/, 'GT'
+  string.gsub! /DG/, 'G'
+  string.gsub! /PH/, 'F'
+  string.gsub! /(.)(?:AH|HA)/, "#{$1}A"
+  string.gsub! /KN/, 'N'
+  string.gsub! /K/, 'C'
+  string.gsub! /(.)M/, "#{$1}N"
+  string.gsub! /(.)Q/, "#{$1}G"
+  string.sub! /(?:SCH|SH)$/, 'S'
+  string.gsub! /YW/, 'Y'
+  
+  string.gsub! /(.)Y(.)/, "#{$1}A#{$2}"
+  string.gsub! /WR/, 'R'
+  
+  string.gsub! /(.)Z/, "#{$1}S"
+  
+  string.sub! /AY$/, 'Y'
+  string.sub! /A+$/, ''
+
+  string.gsub! /(\w)\1+/, "#{$1}"
+  
+  if first =~ /[AEIOU]/
+    string = first + string[1..-1]
+  end
+    
+  string
+end
+
+puts nysiis ARGV[0]
