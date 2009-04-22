3338d8edcd5e64c5b44b11fd9a747e52766df1b2
Kyle Burton <kyle.burton@gmail.com>
2009-04-22 13:14:58 -0400

    added default controller

--- edist-app/app/controllers/index_controller.rb
+++ edist-app/app/controllers/index_controller.rb
@@ -1,2 +1,6 @@
 class IndexController < ApplicationController
+
+  def index
+  end
+
 end
--- edist-app/config/routes.rb
+++ edist-app/config/routes.rb
@@ -33,6 +33,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
+  map.root :controller => "index"
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
