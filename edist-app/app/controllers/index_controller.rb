2186813937ad6a5d1ce2846f16c10ba4f0256a14
Kyle Burton <mortis@indigo64.(none)>
2009-04-23 22:48:39 -0400

    jquery and instant feedback

--- edist-app/app/controllers/index_controller.rb
+++ edist-app/app/controllers/index_controller.rb
@@ -9,9 +9,8 @@ class IndexController < ApplicationController
   end
 
   def edist
+    set_params
     edist = Edist.new
-    @left_string  = params[:left_string]  || ''
-    @right_string = params[:right_string] || ''
     @edits, @matrix = edist.distance(@left_string,@right_string)
     len = @left_string.length
     len = @right_string.length if @right_string.length > len
@@ -20,7 +19,47 @@ class IndexController < ApplicationController
 
 
   def brew
-    brew = Brew.new
+    set_params
+    compute_brew_grid
+  end
+
+  def brew_grid
+    set_params
+    compute_brew_grid
+    render :partial => "brew_grid", :layout => false
+  end
+
+  def phonetic_results
+    set_params
+    render :partial => "phoentic_results", :layout => false
+  end
+
+  def title
+    set_params
+    compute_brew_grid
+    render :partial => "title", :layout => false
+  end
+
+private
+  def compute_brew_grid
+    the_brew = Brew.new
+    @score, @matrix, @traceback = the_brew.distance(@left_string,@right_string,
+                                                :initial => @initial_cost.to_f,
+                                                :match   => @match_cost.to_f,
+                                                :insert  => @insert_cost.to_f,
+                                                :delete  => @delete_cost.to_f,
+                                                :subst   => @subst_cost.to_f)
+    len = @left_string.length
+    len = @right_string.length if @right_string.length > len
+    @max_penalty = [@initial_cost.to_f,
+                   @match_cost.to_f,
+                   @insert_cost.to_f,
+                   @delete_cost.to_f,
+                   @subst_cost.to_f].max * len.to_f
+    @brew_score = (@max_penalty - @score.to_f) / @max_penalty
+  end
+
+  def set_params
     @left_string  = params[:left_string]  || 'BRYN'
     @right_string = params[:right_string] || 'RESTAURANT'
     @initial_cost = params[:initial_cost] || 0.0
@@ -28,15 +67,7 @@ class IndexController < ApplicationController
     @insert_cost  = params[:insert_cost]  || 0.1
     @delete_cost  = params[:delete_cost]  || 15.0
     @subst_cost   = params[:subst_cost]   || 1.0
-    @score, @matrix, @traceback = brew.distance(@left_string,@right_string,
-                                                :initial => @initial_cost.to_f,
-                                                :match   => @match_cost.to_f,
-                                                :insert  => @insert_cost.to_f,
-                                                :delete  => @delete_cost.to_f,
-                                                :subst   => @subst_cost.to_f)
-    
-    len = @left_string.length
-    len = @right_string.length if @right_string.length > len
-    @brew_score = (len.to_f - @score.to_f) / len.to_f
   end
+
+
 end
